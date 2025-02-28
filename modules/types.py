9b4973c340fbed483eb59c871c9b4370a2279760
Igor Korovchenko <igsekor@gmail.com>
2025-02-28 14:57:30 +0300

    Add new simple methods and refactor comments

--- modules/types.py
+++ modules/types.py
@@ -1,15 +1,30 @@
 from config.constants import MONTH_NUMBER
 
+# Tuple [day_count, month_abbr, year]
 OwnDate = tuple[int, str, int]
+
+
+def init_date(day, month_abr, year) -> OwnDate:
+  if month_abr in MONTHS:
+    return (day, month_abr, year)
+  raise ValueError("var 'month_abr' should be from the list MONTHS")
+
 
 def get_day_num(date: OwnDate) -> int:
   return date[0]
 
+
+def get_week_day_num(week_abr) -> int:
+  return WEEK.index(week_abr)
+
+
 def get_month_num(date: OwnDate) -> int:
-  return MONTH_NUMBER[date[1]]
+  return MONTHS.index(date[1])
+
 
 def get_month_abr(date: OwnDate) -> str:
   return date[1]
+
 
 def get_year_num(date: OwnDate) -> int:
   return date[2]
