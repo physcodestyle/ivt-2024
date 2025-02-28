2b4faa0369d25341ace30a36420d486b26d4e6ef
Igor Korovchenko <igsekor@gmail.com>
2025-02-28 16:37:31 +0300

    Add conversions for Unix Time Stamp

--- modules/time.py
+++ modules/time.py
@@ -1,12 +1,33 @@
 from modules.own_time import OwnTime, init_time, init_utc_time_h, init_utc_time_hm, init_utc_time_hms, get_hours, get_minutes, get_seconds
 
 
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOURS = 60 * 60
+
+
 def to_unix_time_stamp(time: OwnTime) -> int:
-  return time[2] + 60 * time[1] + 60 * 60 * time[0]
+  return time[2] + SECONDS_IN_MINUTE * time[1] + SECONDS_IN_HOURS * time[0]
+
+
+def to_hms(seconds_from_midnight: int) -> tuple[int, int, int]:
+  hours = seconds_from_midnight // SECONDS_IN_HOURS
+  minutes = (seconds_from_midnight % SECONDS_IN_HOURS) // SECONDS_IN_MINUTE
+  seconds = (seconds_from_midnight % SECONDS_IN_HOURS) % SECONDS_IN_MINUTE
+  return (hours, minutes, seconds)
+
+
+def to_own_time(seconds_from_midnight: int, timezone_hours: int, timezone_minutes: int) -> OwnTime:
+  hours, minutes, seconds = to_hms(seconds_from_midnight)
+  return init_time(hours, minutes, seconds, timezone_hours, timezone_minutes)
+
+
+def to_own_time_utc(seconds_from_midnight: int) -> OwnTime:
+  hours, minutes, seconds = to_hms(seconds_from_midnight)
+  return init_utc_time_hms(hours, minutes, seconds)
 
 
 def to_unix_time_stamp_from_utc(time: OwnTime) -> int:
-  return to_unix_time_stamp(time) + 60 * time[4] + 60 * 60 * time[3]
+  return to_unix_time_stamp(time) + SECONDS_IN_MINUTE * time[4] + SECONDS_IN_HOURS * time[3]
 
 
 def get_interval_in_seconds(start: OwnTime, end: OwnTime) -> OwnTime:
